5c35b4aec96cf0dfbbbb141cc518090c3a46af29
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2023-02-08 13:41:25 +0600

    Added validation for inputs

--- homework1/src/component/Button/Button.js
+++ homework1/src/component/Button/Button.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Button.css";
-
 export class Button extends React.Component {
-    render() {
-      return (
-        <>
-            <button className="btn">{this.props.title}</button>
-        </>
-      );
-    }
+  render() {
+    return (
+      <button className="btn" type={this.props.type}>
+        {this.props.title}
+      </button>
+    );
   }
-  
+}
